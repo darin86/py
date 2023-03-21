a4cb7c5b4c3768e9cecc810ddf248bd336307e15
Darling86 <1570906137@qq.com>
2023-03-21 08:33:52 +0800

    基础课

--- passwd.py
+++ passwd.py
@@ -6,4 +6,16 @@ def encrypto(password):
         crypto = crypto+"".join(chr((ord(j)-97+3)%26+97))
     return crypto
 name="root"
-passwd=""
+passwd="urrw"
+i=0
+while(i<3):
+    username = input("请输入你的用户名:")
+    password = getpass.getpass("请输入你的密码：")
+    i+=1
+    if username==name and encrypto(password)==passwd:
+        print("欢迎{}进入Python学习系统".format(username))
+        break
+    else:
+        print("用户名或密码错误,请再次输入！")
+else:
+    print("你尝试超过3次,谢谢使用,再见！")
