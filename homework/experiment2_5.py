8f02154709b21b257e14d1c0bca265e58f18050c
darin <1570906137@qq.com>
2023-04-08 18:48:20 +0800

    experiment

--- homework/experiment2_5.py
+++ homework/experiment2_5.py
@@ -1,19 +1,14 @@
-while(1):
-    a=input("请输入你的百分制成绩:")
-    if a>=80 and a<=100:
-        print("A")
-        break
-    elif a>=60 and a<=80:
-        print("B")
-        break
-    elif a>=40 and a<=60:
-        print("C")
-        break
-    elif a>=20 and a<=40:
-        print("D")
-        break
-    elif a>=0 and a<=20:
-        print("E")
-        break
-    else:
-        print("输入有误，请再次输入:")
+score = int(input("请输入成绩: "))
+
+if score >= 90:
+    grade = 'A'
+elif score >= 80:
+    grade = 'B'
+elif score >= 70:
+    grade = 'C'
+elif score >= 60:
+    grade = 'D'
+else:
+    grade = 'E'
+
+print("您的成绩等级为：", grade)
