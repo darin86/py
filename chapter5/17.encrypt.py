ec1e65a09b33ad5af79c257785eeb19ec8f08662
darin <1570906137@qq.com>
2023-04-11 16:03:47 +0800

    clear

--- chapter5/17.encrypt.py
+++
@@ -1,5 +0,0 @@
-passwd = 'asdf'
-crypto =""
-for j in passwd:
-    crypto = crypto + "".join(chr((ord(j)-97+3)%26+97))
-print(crypto)
--- chapter5/6.express.py
+++
@@ -1,9 +0,0 @@
-import math
-a=1
-b=-3
-c=2
-delta = b**2-4*a*c
-print(delta)
-x1=(-b+math.sqrt(delta))/(2*a)
-x2=(-b-math.sqrt(delta))/(2*a)
-print(x1,x2)
